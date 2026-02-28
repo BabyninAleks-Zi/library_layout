3df96ae30758832f806d98ee54e08d022cf537a1
Aleksandr Babynin <babynin.home@gmail.com>
2026-02-28 22:41:09 +1100

    Split books by two columns

--- render_website.py
+++ render_website.py
@@ -1,5 +1,6 @@
 import json
 from livereload import Server
+from more_itertools import chunked
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 
@@ -9,21 +10,22 @@ env = Environment(
 )
 
 
-def build():
+def on_reload():
     template = env.get_template("template.html")
     with open("meta_data.json", "r", encoding="utf-8") as my_file:
         books = json.load(my_file)
 
-    rendered_page = template.render(books=books)
+    books_by_row = list(chunked(books, 2))
+    rendered_page = template.render(books_by_row=books_by_row)
 
     with open("index.html", "w", encoding="utf-8") as file:
         file.write(rendered_page)
 
 
 if __name__ == "__main__":
-    build()
+    on_reload()
 
     server = Server()
-    server.watch("template.html", build)
-    server.watch("meta_data.json", build)
-    server.serve(root=".", host="0.0.0.0", port=8000, default_filename="index.html")
+    server.watch("template.html", on_reload)
+    server.watch("meta_data.json", on_reload)
+    server.serve(root=".", default_filename="index.html")
